bf2337107bcf0a99778403d9236b0389d06753ac
yanyifan <yyf020517@hotmail.com>
2025-05-21 13:32:39 +0800

    update: train mt5

--- src/scripts/stat.py
+++ src/scripts/stat.py
@@ -4,8 +4,8 @@ from tqdm import tqdm
 
 
 if __name__ == '__main__':
-    file_pattern = '/root/autodl-tmp/datasets/csl-news/poses/archive_*/*.npy'
-    # file_pattern = '/root/autodl-tmp/datasets/csl-news/pred_poses/archive_*/*.npy'
+    # file_pattern = '/root/autodl-tmp/datasets/csl-news/poses/archive_*/*.npy'
+    file_pattern = '/root/autodl-tmp/datasets/csl-news/pred_poses/archive_*/*.npy'
     # file_pattern = '/root/autodl-tmp/datasets/csl-daily/sentence/poses/*.npy'
     # file_pattern = '/root/autodl-tmp/datasets/csl-daily/sentence/pred_poses/*.npy'
 
