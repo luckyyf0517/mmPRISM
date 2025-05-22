b62bfdb5b6a369706b6598b9cc055a22f5be7988
yanyifan <yyf020517@hotmail.com>
2025-05-22 12:48:00 +0800

    update: rename news

--- scripts/train_mt5_news.sh
+++ scripts/train_mt5_news.sh
@@ -2,11 +2,11 @@
 deepspeed --include localhost:0,1 \
 	--master_port 1234 \
     run_peft.py \
-    --config config/wavellm_mt5_news_pose.yaml \
+    --config config/wavellm_mt5_news.yaml \
     --batch-size 16 \
     --max-epochs 5 \
     --gradient-accumulation-steps 8 \
-    --version "wavellm_mt5_gt_pose_0521_v0" \
+    --version "wavellm_mt5_gt_0521_v0" \
     --dtype bf16 \
     --zero_stage 2 \
     --resume-checkpoint "log/peft_finetune/wavellm_mt5_pose_0515/last.ckpt" \
