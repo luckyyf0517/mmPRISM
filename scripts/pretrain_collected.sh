38c1ad06b079f5339e2c313a196dbbed271cf86c
yanyifan <yyf020517@hotmail.com>
2025-08-13 13:47:55 +0800

    update: new training

--- scripts/pretrain_collected.sh
+++ scripts/pretrain_collected.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 torchrun --nproc_per_node=2 \
-    --master_port 12350 \
+    --master_port 12345 \
     run_model.py \
-    --config config/omnihand_mmhand_collected.yaml \
+    --config config/omnihand_rtm_collected.yaml \
     --batch-size 32 \
-    --max-epochs 100 \
-    --version "omnihand-mmhand-collected-0707" \
+    --max-epochs 25 \
+    --version "omnihand-rtm-collected-no-disc-0813" \
     --precision 32 \
     --reset
-    # --test
